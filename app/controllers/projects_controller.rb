d483293d95e8e5c1b63ab867867bc6c8446b22bd
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2016-01-23 13:38:22 +0000

    add create action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,16 @@
 class ProjectsController < ApplicationController
+  permits :name
+
   def index
     @projects = Project.all
   end
 
   def new
     @project = Project.new
+  end
+
+  def create(project: )
+    @project = Project.create!(project)
+    redirect_to projects_path
   end
 end
