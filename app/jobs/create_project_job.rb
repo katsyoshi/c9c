35a9c57fa0703a22cbf63d252c916ee3a24e206e
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2016-01-24 15:28:26 +0000

    create job and spec job

--- app/jobs/create_project_job.rb
+++ app/jobs/create_project_job.rb
@@ -2,6 +2,25 @@ class CreateProjectJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    args.each do |params|
+      make_project_dir(params)
+      make_project(params)
+    end
+  end
+
+  private
+
+  def make_project_dir(user: 'hoge', project:)
+    FileUtils.mkdir_p(project_root_dir(user))
+  end
+
+  def make_project(template: 'master', user: 'hoge', project:)
+    origin_files = project_root_dir('template', template)
+    FileUtils.cp_r(origin_files, project_root_dir(user, project.name))
+  end
+
+  def project_root_dir(*project_dir)
+    root = Setting.root
+    root.first == 'Rails.root' ? Rails.root.join(*root[1..-1], *project_dir) : root.join('/')
   end
 end
--- spec/jobs/create_project_job_spec.rb
+++ spec/jobs/create_project_job_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe CreateProjectJob, :type => :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'create proejct' do
+    let(:dummy) { Project.create(name: 'hoge') }
+    let(:project_dir) { Rails.root.join('spec', 'project', 'hoge', 'hoge') }
+
+    before { CreateProjectJob.perform_now(project: dummy) }
+    after do
+      FileUtils.rm_rf Rails.root.join('spec', 'project', 'hoge')
+      Project.delete_all
+    end
+
+    it 'exsit dir' do
+      expect(File).to exist(project_dir)
+    end
+
+    it 'exist project file' do
+      expect(File).to exist(project_dir.join('Gemfile'))
+    end
+  end
 end
